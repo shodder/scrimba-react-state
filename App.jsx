7d6234244d569f251a44dd31ddb1517de7e87e9a
Simon Hodder <2083432+shodder@users.noreply.github.com>
2025-09-17 20:06:33 +0100

    Proper way to manage state across multiple children

--- App.jsx
+++ App.jsx
@@ -12,15 +12,38 @@ export default function App(props) {
     //     backgroundColor: btnColour
     // }
 
-    /**
-     * Challenge part 3:
-     * Our buttons got turned off by default! Update the code
-     * so if the button is "on", it has the className of "on".
-     */
-    
+    // map over the maps array and if the current item
+    // has the same ID as the one pass to toggle, then
+    // flip its value
+    function toggle(btnId) {
+        console.log(`Clicked! ${btnId}`)
+        /**
+         * Challenge:
+         * Call setPads to update the state of the one pad that was
+         * clicked. Map over the previous pads array, and if the current
+         * item you're iterating over has the same id as the `id` passed
+         * to this function, then return a new object with the `on` value
+         * set to the opposite of what it was before.
+         * Otherwise (if the ids don't match), just return the previous
+         * item as it was, unchanged.
+         */
+        setPads(
+            prevPads => prevPads.map(
+                item => {
+                    return item.id === btnId ? {
+                        ...item,
+                        on: !item.on
+                    } : item
+                }
+            )
+        )
+
+    }
     
     const buttonElements = pads.map(pad => (
-            <Pad key={pad.id} color={pad.color} isOn={pad.on} />
+            <Pad key={pad.id} btnId={pad.id} color={pad.color} isOn={pad.on} handleToggle={toggle}/>
+            // can use key in components as that is a React specific attribute so to pass an id down
+            // just add another prop with the id to pass down. In this case btnId
         )
     )
 
--- Pad.jsx
+++ Pad.jsx
@@ -1,22 +1,20 @@
-import { useState } from "react"
 
 export default function Pad(props) {
-    const [padOn, setPadOn] = useState(props.isOn)
     
-    const isOnSyle = padOn ? "on" : undefined
-
-    function handleClick() {
-        setPadOn(
-            prevPadOn => !prevPadOn)
-    }
+    console.log(`props.btnId? ${props.btnId}`)
+    console.log(`props.IsOn? ${props.isOn}`)
+    const isOnSyle = props.isOn ? "on" : undefined
 
     const btnStyle = {
         backgroundColor: props.color,
     }
 
-    console.log(isOnSyle)
-
+    console.log(`IsOn? ${isOnSyle}`)
     return (
-        <button className={isOnSyle} style={btnStyle} key={props.key} onClick={handleClick}></button>
+        <button className={isOnSyle} style={btnStyle} key={props.key} onClick={() => props.handleToggle(props.btnId)}></button>
+        // you have to use an anon function to pass function parameters to dom event handlers
+        // normally onClick=function(event) is what is actually happening so you can pass your own param
+        // instead use an anon onClick={(event) => function(WHATEVER YOU LIKE :)) - this works
+        // you don't explicity put event in so onClick={() => function(WHATEVER YOU LIKE :))
     )
 }
